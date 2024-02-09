f7477e5b17ac327be9b42801a768ecb842fa69f0
aa7381 <aa7381@bs.amalnet.k12.il>
2024-02-09 21:14:02 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.demo23120410;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.R;
+
+public class MainActivity extends AppCompatActivity {
+    Button btn;
+    int mone=0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn = findViewById(R.id.btn);
+    }
+
+    // method that start when button clicked
+    public void clicked(View view) {
+        mone+=1;
+        btn.setText("This is a click number:"+mone);
+    }
+}
